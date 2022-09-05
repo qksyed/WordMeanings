53ab937121ca805bcd3f8ad4dcbb1d30eb45c3d8
qksyed <syedqadirkamran@yahoo.com>
2022-09-04 20:09:58 -0400

    Final Project Completed

--- src/Words.java
+++ src/Words.java
@@ -1,21 +1,37 @@
-import java.io.File;
+import java.io.*;
+import java.util.Scanner;
 
 public class Words {
-	public void doesFileExist(String path) {
-		File wordFile = new File("c:/temp/temp.txt");
+	public static boolean doesFileExist(String path) {
+		File wordFile = new File(path);
 		boolean exists = wordFile.exists();
-		if (exists) {
+		
+		return exists;
+	}
+	
+
+	public static void main(String[] args) throws IOException, FileNotFoundException {
+		System.out.println("Please provide the class path to check the file existence and to read!: ");
+		Scanner filePath = new Scanner(System.in);
+		String userFilePath = filePath.nextLine();
+		boolean there = false;
+		
+		//String userFilePath = "src/wordMeanings.txt";
+		there = doesFileExist(userFilePath);
+		if (there) {
 			System.out.println("File exists");
 		}
 		else {
 			System.out.println("No susch file exists at this location");
 		}
-	}
-	
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		BufferedReader br = new BufferedReader(new FileReader(userFilePath));
+		String st;
+		while ((st = br.readLine()) != null) {
+			String[] arrOfStr = st.split("-");
+			for (String a : arrOfStr) {
+				System.out.println(a);
+			}
+		}
 	}
 
 }
